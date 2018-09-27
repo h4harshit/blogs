a5854361b597872b458c2e51df4ad505d0a22fd2
Harshit Sharma <harshit.sharma@tieto.com>
2018-09-27 19:29:57 +0530

    styled components added

--- src/components/header.js
+++ src/components/header.js
@@ -1,33 +1,32 @@
 import React from 'react'
 import { Link } from 'gatsby'
+import styled from 'styled-components'
+
+const HeaderContainer = styled.div`
+  display: flex;
+  background: #8c2b5e;
+  height: 5rem;
+  padding-top: 1.5rem;  
+  margin-bottom: 2rem;
+`
+const LinkContainer = styled.h2`
+  padding-left: 2rem;
+`
 
 const Header = ({ siteTitle }) => (
-  <div
-    style={{
-      background: 'rebeccapurple',
-      marginBottom: '1.45rem',
-    }}
-  >
-    <div
-      style={{
-        margin: '0 auto',
-        maxWidth: 960,
-        padding: '1.45rem 1.0875rem',
-      }}
-    >
-      <h1 style={{ margin: 0 }}>
-        <Link
-          to="/"
-          style={{
-            color: 'white',
-            textDecoration: 'none',
-          }}
-        >
-          {siteTitle}
-        </Link>
-      </h1>
-    </div>
-  </div>
+  <HeaderContainer>
+    <LinkContainer>
+      <Link
+        to="/"
+        style={{
+          color: 'white',
+          textDecoration: 'none',
+        }}
+      >
+        {siteTitle}
+      </Link>
+    </LinkContainer>
+  </HeaderContainer>
 )
 
 export default Header
