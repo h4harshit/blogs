035cf5dc814cc13bfd0c4937be432073055ebc6d
Harshit Sharma <harshit.sharma@tieto.com>
2018-09-30 16:16:48 +0530

    Home page style changes

--- src/pages/index.js
+++ src/pages/index.js
@@ -12,25 +12,43 @@ const Excerpt = styled.p`
   text-align: justify;
 `
 
+const BlogContainer = styled.div`
+  background-color: #eeeeee;
+  margin-top: 2rem;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  height: 11rem;
+`
+
+const Blog = styled.div`
+  width: 80%;
+`
+
 export default ({ data }) => {
   return (
     <Layout>
       <div>
         {data.allMarkdownRemark.edges.map(({ node }) => (
           <div key={node.id}>
-            <Link
-              to={node.fields.slug}
-              className={css`
-                text-decoration: none;
-                color: inherit;
-              `}
-            >
-              <h3>
-                {node.frontmatter.title}{' '}
-                <Date>&mdash; {node.frontmatter.date}</Date>
-              </h3>
-              <Excerpt>{node.excerpt}</Excerpt>
-            </Link>
+            <BlogContainer>
+              <Blog>
+                <Link
+                  to={node.fields.slug}
+                  className={css`
+                    text-decoration: none;
+                    color: inherit;
+                    padding: 0 2rem;
+                  `}
+                >
+                  <h3>
+                    {node.frontmatter.title}{' '}
+                    {/* <Date>&mdash; {node.frontmatter.date}</Date> */}
+                  </h3>
+                  <Excerpt>{node.excerpt}</Excerpt>
+                </Link>
+              </Blog>
+            </BlogContainer>
           </div>
         ))}
       </div>
